5ca510b724536220d99b1c7d0f6b2bed6acfff17
Fagner <fagnerfpr@gmail.com>
2021-06-01 15:16:55 -0300

    change worker to use real api

--- app/workers/fetch_latest_quotation_worker.rb
+++ app/workers/fetch_latest_quotation_worker.rb
@@ -3,8 +3,7 @@ class FetchLatestQuotationWorker
   sidekiq_options queue: 'default'
 
   def perform
-    Quotation.create(real: (1..10).to_a.sample, us_dollar: (1..10).to_a.sample, aus_dollar: (1..10).to_a.sample, euro: (1..10).to_a.sample)
-    #service = FetchLatestQuotationService.new
-    #service.call!
+    service = FetchLatestQuotationService.new
+    service.call!
   end
 end
