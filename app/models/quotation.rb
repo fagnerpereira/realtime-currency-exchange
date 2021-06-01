05fb8f932a6cf448e4b388855c6c44114b865b5e
Fagner <fagnerfpr@gmail.com>
2021-06-01 13:00:55 -0300

    add turbo stream to update the graphic

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -3,4 +3,12 @@ class Quotation < ApplicationRecord
   monetize :us_dollar_cents, allow_nil: true
   monetize :aus_dollar_cents, allow_nil: true
   monetize :euro_cents, allow_nil: true
+
+  after_create_commit -> do
+    broadcast_append_to :quotations
+  end
+
+  after_destroy_commit -> do
+    broadcast_remove_to :quotations
+  end
 end
